09651aa712141307eb3579dea6c6dcb502729534
Janusz Skonieczny <janusz.skonieczny@gmail.com>
2014-05-27 22:53:54 +0200

    0.5 published on pypi

--- setup.py
+++ setup.py
@@ -7,17 +7,22 @@ from pip.req import parse_requirements
 
 install_requires = parse_requirements(os.path.join(os.path.dirname(__file__), "requirements.txt"))
 
+with open('README') as readme:
+    long_description = readme.read()
+
 setup_kwargs = {
     'name': "flask-social-blueprint",
-    'version': "0.6",
+    'version': "0.5",
     'packages': find_packages("src"),
     'package_dir': {'': 'src'},
     'install_requires': [str(r.req) for r in install_requires],
 
     # metadata for upload to PyPI
     'author': "Janusz Skonieczny",
+    'author_email': "js@bravelabs.pl",
     # 'author_email': "",
     'description': "An OAuth based authentication blueprint for flask. Easy to extend and override",
+    'long_description': long_description,
     'license': "MIT",
     'keywords': "flask social oauth authentication",
     'url': "https://github.com/wooyek/flask-social-blueprint",
